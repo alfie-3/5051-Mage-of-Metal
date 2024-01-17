63f027c17af04cee39ebef190ed47dc429b911b4
AdrienMoullec <s4209664@glos.ac.uk>
2024-01-17 13:34:27 +0000

    FMOD: 1st parameter is cool
    Next going to work on music notes to hit.

--- Assets/Scripts/AudioControl/AudioManager.cs
+++ Assets/Scripts/AudioControl/AudioManager.cs
@@ -1,21 +1,69 @@
+using FMOD.Studio;
 using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class AudioManager : MonoBehaviour
-{
-    [SerializeField] public List<EventReference> soundEvent;
-    [SerializeField] public float rate;
-    [SerializeField] public GameObject player;
+public class AudioManager : MonoBehaviour {
 
-    private void Start()
+    public static AudioManager managerInstance { get; private set; }
+    [SerializeField] EventReference audioRef;
+    EventInstance instance;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float musicPercentage;
+
+    private void Awake()
     {
-        PlaySound();
+        if (managerInstance == null)
+        {
+            managerInstance = this;
+        }
     }
 
-    public void PlaySound()
+    public void MusicPlay()
     {
-        RuntimeManager.PlayOneShotAttached(soundEvent[0], player);
+        instance = FMODUnity.RuntimeManager.CreateInstance(audioRef);
+        instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+        instance.start();
+        //RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+    public void AddScore()
+    {
+        musicPercentage += 0.1f;
+        instance.setParameterByName("InstrumentAudio", musicPercentage);
+    }
+    public void RemoveScore()
+    {
+        musicPercentage -= 0.1f;
+        instance.setParameterByName("InstrumentAudio", musicPercentage);
     }
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(AudioManager))]
+public class AudioManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AudioManager audioManager = (AudioManager)target;
+        DrawDefaultInspector();
+        
+        if (GUILayout.Button("Start Music"))
+        {
+            audioManager.MusicPlay();
+        }
+        if (GUILayout.Button("Add Score"))
+        {
+            audioManager.AddScore();
+        }
+        if (GUILayout.Button("Remove Score"))
+        {
+            audioManager.RemoveScore();
+        }
+    }
+}
+#endif
--- Assets/Scripts/AudioControl/AudioManagerEditor.cs
+++ Assets/Scripts/AudioControl/AudioManagerEditor.cs
@@ -1,19 +1,3 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-/*
-[CustomEditor(typeof(AudioManager))]
-public class AudioManagerEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        AudioManager audioManager = (AudioManager)target;
-        audioManager.
-        
-        if (GUILayout.Button("Start Audio"))
-        {
-            audioManager.PlaySound();
-        }
-    }
-}*/
