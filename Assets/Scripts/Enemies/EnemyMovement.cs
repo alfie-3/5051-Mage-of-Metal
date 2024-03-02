009edada5f1cd23e31cfa04f9233b6b020ff3d93
AdrienMoullec <s4209664@glos.ac.uk>
2024-03-02 13:56:36 +0000

    Working enemymovement
    Also started menu

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -3,12 +3,26 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public NavMeshAgent agent;
+    NavMeshAgent agent;
+    [SerializeField] float minAttackDistance;
+
     GameObject player;
+    float distance = Mathf.Infinity;
 
     private void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance( transform.position , player.transform.position );
+        Debug.Log(distance);
+        if (distance < minAttackDistance)
+        {
+            agent.SetDestination(player.transform.position);
+        }
     }
 
 }
