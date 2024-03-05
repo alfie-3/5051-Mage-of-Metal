a42d18674a3c5b7bf7a1daf94abfafa2f1dee849
Alfie Elliott <116003344+alfie-3@users.noreply.github.com>
2024-03-05 20:24:32 +0000

    Revert "Merge branch 'main' of https://github.com/alfie-3/5051-Mage-of-Metal"
    This reverts commit 69d285cdd62bea00f411e10b8973dd7d71d7975a, reversing
    changes made to c9263bfc252c6aec73104705bceedc6184d08732.

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,4 +1,3 @@
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Splines;
@@ -8,16 +7,12 @@ public class EnemyMovement : MonoBehaviour
     NavMeshAgent agent;
     [SerializeField] float minAttackDistance;
 
-    ISpline spline;
     GameObject player;
     float distance = Mathf.Infinity;
 
 
     private void Start()
     {
-        spline = GameObject.Find("Spline").GetComponent<ISplineContainer>().Splines[0];
-
-
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
     }
@@ -27,30 +22,8 @@ public class EnemyMovement : MonoBehaviour
         distance = Vector3.Distance( transform.position , player.transform.position );
         if (distance < minAttackDistance)
         {
-            //agent.SetDestination(player.transform.position);
-            /*if (spline != null)
-            {
-                Debug.Log("spline is a thing");
-            }
-            else
-            {
-
-            }*/
-
-            SplineUtility.GetNearestPoint(spline, new float3(transform.position.x , transform.position.y, transform.position.z), out float3 nearest, out float t);
-            Vector3 point = new Vector3(nearest.x, nearest.y, nearest.z);
-            float distance = Vector3.Distance(transform.position, point);
-
-            if (distance < 2)
-            {
-                agent.SetDestination(player.transform.position);
-            }
-            else
-            {
-                agent.SetDestination(point);
-            }
+            agent.SetDestination(player.transform.position);
         }
     }
-
 
 }
