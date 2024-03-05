14e501115373554bf94a77e7baea5c7545de6837
AdrienMoullec <s4209664@glos.ac.uk>
2024-03-05 20:12:11 +0000

    Enemies go to Place yes

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Splines;
@@ -7,12 +8,16 @@ public class EnemyMovement : MonoBehaviour
     NavMeshAgent agent;
     [SerializeField] float minAttackDistance;
 
+    ISpline spline;
     GameObject player;
     float distance = Mathf.Infinity;
 
 
     private void Start()
     {
+        spline = GameObject.Find("Spline").GetComponent<ISplineContainer>().Splines[0];
+
+
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
     }
@@ -22,8 +27,30 @@ public class EnemyMovement : MonoBehaviour
         distance = Vector3.Distance( transform.position , player.transform.position );
         if (distance < minAttackDistance)
         {
-            agent.SetDestination(player.transform.position);
+            //agent.SetDestination(player.transform.position);
+            /*if (spline != null)
+            {
+                Debug.Log("spline is a thing");
+            }
+            else
+            {
+
+            }*/
+
+            SplineUtility.GetNearestPoint(spline, new float3(transform.position.x , transform.position.y, transform.position.z), out float3 nearest, out float t);
+            Vector3 point = new Vector3(nearest.x, nearest.y, nearest.z);
+            float distance = Vector3.Distance(transform.position, point);
+
+            if (distance < 2)
+            {
+                agent.SetDestination(player.transform.position);
+            }
+            else
+            {
+                agent.SetDestination(point);
+            }
         }
     }
+
 
 }
