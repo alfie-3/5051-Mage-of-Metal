94eb71c0afcd42d6d16337906ae068b35cf7eef0
ALadysz <s4204218@glos.ac.uk>
2024-04-22 04:06:42 +0100

    Song Progress Bar
    fmod is scary

---
+++ Assets/Scripts/UI/SongProgress.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using FMOD.Studio; 
+using FMODUnity;
+
+public class SongProgress : MonoBehaviour
+{
+    [SerializeField] private Slider progressSlider;
+    public AudioManager audioManagerScript;
+    private EventInstance musicEvent; 
+    private int songLength; 
+    private EventDescription eventDesc;
+
+
+    void Start()
+    {
+        //yoink song from audio manager
+        musicEvent = audioManagerScript.instance;
+        //get description of event
+        musicEvent.getDescription(out eventDesc);
+        //get length from desc
+        eventDesc.getLength(out songLength);
+        //set max of slider to total song length
+        progressSlider.maxValue = songLength;
+    }
+
+    void Update()
+    {
+        //if the song exists/is currently being played
+        if (musicEvent.isValid())
+        {
+            //get current place on track
+            musicEvent.getTimelinePosition(out int position);
+            //set slider to progress
+            progressSlider.value = position;
+        }
+    }
+}
