db4dbb70fe2ae10945155ee2a26b0d4f87f0d97d
ALadysz <s4204218@glos.ac.uk>
2024-04-24 08:49:33 +0100

    How to play + slight tweaks

--- Assets/Scripts/UI/HowToPlay.cs
+++ Assets/Scripts/UI/HowToPlay.cs
@@ -7,7 +7,14 @@ public class HowToPlay : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject MainMenuCanvas;
     [SerializeField] private GameObject HTPCanvas;
-
+    [SerializeField] private GameObject DisplayOne;
+    [SerializeField] private GameObject DisplayTwo;
+    [SerializeField] private GameObject DisplayThree;
+    private int currentlyActive;
+    private void Start()
+    {
+        currentlyActive = 1;
+    }
     public void SwitchCanvi()
     {
         //animator.Play("animate");
@@ -31,6 +38,54 @@ public class HowToPlay : MonoBehaviour
         }
     }
 
+public void SwitchDisplayLeft()
+{
+    switch (currentlyActive)
+    {
+        case 1:
+            currentlyActive = 3;
+            DisplayOne.SetActive(false);
+            DisplayThree.SetActive(true);
+            break;
+        case 2:
+            currentlyActive = 1;
+            DisplayTwo.SetActive(false);
+            DisplayOne.SetActive(true);
+            break;
+        case 3:
+            currentlyActive = 2;
+            DisplayThree.SetActive(false);
+            DisplayTwo.SetActive(true);
+            break;
+        default:
+            Debug.Log("Something is wrong");
+            break;
+    }
+}
+public void SwitchDisplayRight()
+{
+    switch (currentlyActive)
+    {
+        case 1:
+            currentlyActive = 2;
+            DisplayOne.SetActive(false);
+            DisplayTwo.SetActive(true);
+            break;
+        case 2:
+            currentlyActive = 3;
+            DisplayTwo.SetActive(false);
+            DisplayThree.SetActive(true);
+            break;
+        case 3:
+            currentlyActive = 1;
+            DisplayThree.SetActive(false);
+            DisplayOne.SetActive(true);
+            break;
+        default:
+            Debug.Log("Something is wrong");
+            break;
+    }
+}
     public void Return()
     {
         float currentSpeed = 1.0f;//animator.speed;
